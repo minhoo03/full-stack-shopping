fe7f93f644e2aef452bd1128798734de585e4941
minhoo03 <minhoo0333@gmail.com>
2021-05-12 22:28:46 +0900

    01. UploadProductPage (Route)

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,6 +7,7 @@ import LoginPage from "./views/LoginPage/LoginPage.js";
 import RegisterPage from "./views/RegisterPage/RegisterPage.js";
 import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer"
+import UploadProductPage from "./views/UploadProductPage/UploadProductPage.js"
 
 //null   Anyone Can go inside
 //true   only logged in user can go inside
@@ -21,11 +22,14 @@ function App() {
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route exact path="/product/upload" component={Auth(UploadProductPage, true)} />
         </Switch>
       </div>
       <Footer />
     </Suspense>
   );
 }
+
+// UploadProductPage는 로그인된 유저만 입장 가능하다 Auth(true)
 
 export default App;
---
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function UploadProductPage() {
+    return (
+        <div>
+            UploadProductPage
+        </div>
+    )
+}

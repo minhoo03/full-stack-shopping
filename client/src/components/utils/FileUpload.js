6ae2909719951536bd957ada504ab71d79d5a8f3
minhoo03 <minhoo0333@gmail.com>
2021-05-20 13:44:59 +0900

    05. Send Image to Server (front/back)

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import Dropzone from 'react-dropzone'
 import { AiOutlinePlus } from 'react-icons/ai'
+import axios from 'axios'
+import { Form } from 'react-bootstrap'
 
 export default function FileUpload() {
+
+    const dropHandler = (files) => {
+
+        let formData = new FormData() // form으로 부터 받은 data.append
+
+        const config = {
+            header: {'content-type': 'multipart/fomr-data'} // backend에 content-type을 미리 알림
+        }
+        formData.append("file", files[0])
+
+        axios.post('/api/product/image', formData, config)
+        .then(response => {
+            if(response.data.success) {
+
+            } else {
+                alert('파일을 저장하는데 실패했습니다.')
+            }
+        })
+
+    }
+
     return (
         <div style={{ display: "flex", justifyContent: "space-between"}}>
-            <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+            <Dropzone onDrop={dropHandler}>
                 {({getRootProps, getInputProps}) => (
                     <section>
                     <div
